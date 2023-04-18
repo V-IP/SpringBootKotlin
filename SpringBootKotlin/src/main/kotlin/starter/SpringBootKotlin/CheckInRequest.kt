d70c2fab724cf3d459032e79af892adba9bb7557
Vincze Iulia-Paula <iulia.vincze@stud.ubbcluj.ro>
2023-04-18 15:04:26 +0300

    Initial commit

---
+++ SpringBootKotlin/src/main/kotlin/starter/SpringBootKotlin/CheckInRequest.kt
@@ -0,0 +1,6 @@
+package starter.SpringBootKotlin
+
+class CheckInRequest {
+    var hotelName: String? = null
+    var noGuests = 0
+}
---
+++ SpringBootKotlin/src/main/kotlin/starter/SpringBootKotlin/Hotel.kt
@@ -0,0 +1,4 @@
+package starter.SpringBootKotlin
+
+class Hotel {
+}
